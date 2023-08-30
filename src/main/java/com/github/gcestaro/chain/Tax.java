48111852c7ea7daca44af0861e706171b2a9459e
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2023-08-30 14:53:17 +0100

    Remove old structure

--- src/main/java/com/github/gcestaro/chain/Tax.java
+++
@@ -1,8 +0,0 @@
-package com.github.gcestaro.chain;
-
-import java.math.BigDecimal;
-
-public interface Tax {
-
-  BigDecimal getValue(BigDecimal amount);
-}
--- src/main/java/com/github/gcestaro/strategy/ProcessorStrategy.java
+++
@@ -1,6 +0,0 @@
-package com.github.gcestaro.strategy;
-
-public interface ProcessorStrategy<D, R> {
-
-  R process(D data);
-}
